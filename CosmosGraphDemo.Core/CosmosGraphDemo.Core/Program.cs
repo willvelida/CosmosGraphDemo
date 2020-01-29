1e3cac530e5687c95fbe9f08e79a91fc25dac9a3
Will Velida <willvelida@hotmail.co.uk>
2020-01-29 20:46:53 +1300

    Adding Core project to repo

---
+++ CosmosGraphDemo.Core/CosmosGraphDemo.Core/Program.cs
@@ -0,0 +1,24 @@
+﻿using ExRam.Gremlinq.Core;
+using Gremlin.Net.Structure;
+using System;
+
+namespace CosmosGraphDemo.Core
+{
+    public class Program
+    {
+        private readonly IGremlinQuerySource _gremlinQuerySource;
+
+        public Program(IGremlinQuerySource gremlinQuerySource)
+        {
+            _gremlinQuerySource = gremlinQuerySource.ConfigureEnvironment(env => env
+                .UseModel(GraphModel.FromBaseTypes<Vertex, Edge>(lookup => lookup
+                .IncludeAssembliesOfBaseTypes())))
+                .UseCosmosDb(new Uri(""), "", "", "");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
